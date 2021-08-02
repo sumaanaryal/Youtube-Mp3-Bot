01550a714c0173b1b7cf1158d72cecf7e68a4647
sumaanaryal <49488019+sumaanaryal@users.noreply.github.com>
2021-08-02 11:32:58 +0545

    Update YoutubeMp3Bot.py

--- YoutubeMp3Bot.py
+++ YoutubeMp3Bot.py
@@ -13,7 +13,7 @@ import os
 from config import Config
 
 bot = Client(
-    'Youtube Audio Bot',
+    'ytmp3music_bot',
     bot_token = Config.BOT_TOKEN,
     api_id = Config.API_ID,
     api_hash = Config.API_HASH
